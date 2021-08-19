4832d8a9eb612147a3be4e5db38da96330f37f00
RGBKnights <master@rgbknights.com>
2021-08-19 01:19:08 -0400

    reset working on new theme

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,26 @@
 <template>
-  <!-- Header -->
-  <header id="header">
-    <img class="logo-icon" src="/favicon.ico" style="" />
-    <a href="/" class="logo">Lotus</a>
-  </header>
-  <!-- Nav -->
-  <!--
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  -->
-  <!-- View -->
-  <div id="view">
-    <router-view :key="$route.fullPath" />
-  </div>
-  <!-- Footer -->
-  <footer id="footer">
-    <div class="inner">
-      <h2>Get In Touch</h2>
-      <ul class="actions">
-        <li>
-          <i class="fab fa-twitter"></i>
-          <a href="https://twitter.com/dotahgv" target="_blank">@DotaHGV</a>
-        </li>
-        <li>
-          <i class="fab fa-reddit-alien"></i>
-          <a href="https://www.reddit.com/r/Abilitydraft/" target="_blank">r/AbilityDraft</a>
-        </li>
-        <li>
-          <i class="fab fa-discord"></i>
-          <a href="https://discord.gg/sSFjabV" target="_blank">Chat on Discord</a>
-        </li>
-        <li>
-          <i class="fab fa-github"></i>
-          <a href="https://github.com/HighGroundVision" target="_blank">We love Open Source</a>
-        </li>
-      </ul>
-    </div>
-    <div class="copyright">
-      <a href="http://highgroundvision.com/" target="_blank">
-        <img src="@/assets/hgv.jpg" style="border-radius: 50%;" />
-      </a>
-      <br />
-      &copy; 2020 High Ground Vision
-    </div>
-  </footer>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
 <style>
-#view {
-  background-size: 100%;
-  background-position: center top;
-  background-repeat: repeat-y;
-  background-image: url("~@/assets/banner.jpg");
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-
-createApp(App)
-    .use(store)
-    .use(router)
-    .use(VueAxios, axios)
-    .mount("#app");
+createApp(App).mount('#app')
